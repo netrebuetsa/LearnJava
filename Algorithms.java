2882ee31ac976a91496b92d3514953fc517b86ff
o.hlynova <olga.hlynova@karuna.group>
2022-06-21 19:41:15 +0300

    algorithms 1.0

--- Algorithms.java
+++ Algorithms.java
@@ -7,10 +7,13 @@ public class Algorithms{
     public static void main(String args[])
     {
 
-       int [] massiv = returnMassiv(10);
+      /* int [] massiv = returnMassiv(10);
         int [] massiv_2 = returnMassiv(15);
 
+       */
 
+        int [] massiv = {1, 3, 5, 7, 8};
+        int [] massiv_2 = {1, 2, 3, 4, 5, 6, 7, 9};
 
        // int[] massiv = {0, 1, 1, 2, 3, 5};
         System.out.println();
@@ -18,9 +21,21 @@ public class Algorithms{
         bubbleSort(massiv);
         System.out.println();
         bubbleSort(massiv_2);
-        int[] massiv_3 = merge(massiv, massiv_2);
+        int[] massiv_3 = intersect(massiv, massiv_2);
         for (int i = 0; i < massiv_3.length; i++) {
             System.out.print(massiv_3[i] + " ");
+        }
+        System.out.println();
+
+        int[] massiv_4 = diff(massiv, massiv_2);
+        for (int i = 0; i < massiv_4.length; i++) {
+            System.out.print(massiv_4[i] + " ");
+        }
+        System.out.println();
+
+        int[] massiv_5 = union(massiv, massiv_2);
+        for (int i = 0; i < massiv_5.length; i++) {
+            System.out.print(massiv_5[i] + " ");
         }
         System.out.println();
     }
@@ -87,5 +102,117 @@ public class Algorithms{
             if (!flag) break;
         }
 
+    }
+    public static int[] diff(int[] massiv, int[] massiv_2) {
+        int[] massiv_result = new int[massiv.length + massiv_2.length];
+        int m1 = 0;
+        int m2 = 0;
+        int i = 0;
+        while ( m1 < massiv.length && m2 < massiv_2.length) {
+
+            if ((massiv[m1] == massiv_2[m2])) {
+                m1++;
+                m2++;
+            }
+            else
+            if (massiv[m1] < massiv_2[m2]) {
+                massiv_result[i] = massiv[m1];
+                m1++;
+                i++;
+            }
+
+            else {
+                massiv_result[i] = massiv_2[m2];
+                m2++;
+                i++;
+            }
+        }
+        while (m1 != massiv.length) {
+            massiv_result[i] = massiv[m1];
+            m1++;
+            i++;
+        }
+        while (m2 != massiv_2.length) {
+            massiv_result[i] = massiv_2[m2];
+            m2++;
+            i++;
+        }
+
+        return massiv_result;
+    }
+
+    public static int[] intersect(int[] massiv, int[] massiv_2) {
+        int[] massiv_result = new int[Math.min(massiv.length, massiv_2.length)];
+        int m1 = 0;
+        int m2 = 0;
+        int i = 0;
+        while ( m1 < massiv.length && m2 < massiv_2.length) {
+
+            if ((massiv[m1] == massiv_2[m2])) {
+                massiv_result[i] = massiv[m1];
+                m1++;
+                m2++;
+                i++;
+            }
+            else
+            if (massiv[m1] < massiv_2[m2]) {
+                m1++;
+
+            }
+
+            else {
+                m2++;
+
+            }
+        }
+       
+
+        return massiv_result;
+    }
+     /*
+        A = {1, 3, 5, 7, 8}
+        B = {1, 2, 4, 6, 7}
+        {1, 2, 3, 4, 5, 6, 7}
+      */
+    public static int[] union(int[] massiv, int[] massiv_2) {
+        int[] massiv_result = new int[massiv.length + massiv_2.length];
+        int m1 = 0;
+        int m2 = 0;
+        int i = 0;
+        while ( m1 < massiv.length && m2 < massiv_2.length) {
+
+            if ((massiv[m1] == massiv_2[m2])) {
+                massiv_result[i] = massiv[m1];
+                m1++;
+                m2++;
+                i++;
+            }
+            else
+            if (massiv[m1] < massiv_2[m2]) {
+                massiv_result[i] = massiv[m1];
+                m1++;
+                i++;
+
+            }
+
+            else {
+                massiv_result[i] = massiv_2[m2];
+                m2++;
+                i++;
+
+            }
+        }
+        while (m1 != massiv.length) {
+            massiv_result[i] = massiv[m1];
+            m1++;
+            i++;
+        }
+        while (m2 != massiv_2.length) {
+            massiv_result[i] = massiv_2[m2];
+            m2++;
+            i++;
+        }
+
+        return massiv_result;
     }
 }
